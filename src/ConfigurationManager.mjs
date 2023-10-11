cac7000979453719631c15f25774445ad9be8421
Mr Stone <pierre.evens16@gmail.com>
2023-10-11 09:32:36 -0400

    Replace Deep by lodash

--- src/ConfigurationManager.mjs
+++ src/ConfigurationManager.mjs
@@ -1,5 +1,5 @@
+import { get, set, has } from 'lodash'
 import { Macroable } from '@stone-js/macroable'
-import { Deep } from './Deep.mjs'
 
 /**
  * Class representing a ConfigurationManager.
@@ -32,8 +32,8 @@ export class ConfigurationManager extends Macroable {
   /**
    * Get the specified configuration value.
    *
-   * @param  {string|object} key
-   * @param  {any}           fallback
+   * @param  {string|string[]|object} key
+   * @param  {any}                    fallback
    * @return {any}
    */
   get (key, fallback = null) {
@@ -41,7 +41,7 @@ export class ConfigurationManager extends Macroable {
       return this.getMany(key)
     }
 
-    return Deep.get(this.#items, key, fallback)
+    return get(this.#items, key, fallback)
   }
 
   /**
@@ -52,31 +52,31 @@ export class ConfigurationManager extends Macroable {
    */
   getMany (keys) {
     const entries = Array.isArray(keys) ? keys.map(v => [v, null]) : Object.entries(keys)
-    return entries.reduce((results, [key, fallback]) => ({ ...results, [key]: Deep.get(this.#items, key, fallback) }), {})
+    return entries.reduce((results, [key, fallback]) => ({ ...results, [key]: get(this.#items, key, fallback) }), {})
   }
 
   /**
    * Determine if the given configuration value exists.
    *
-   * @param  {string} key
+   * @param  {string|string[]} key
    * @return {boolean}
    */
   has (key) {
-    return Deep.has(this.#items, key)
+    return has(this.#items, key)
   }
 
   /**
    * Set a given configuration value.
    *
-   * @param  {string|object} key
-   * @param  {any}           value
+   * @param  {string|string[]|object} key
+   * @param  {any}                    value
    * @return {this}
    */
   set (key, value = null) {
     key = typeof key === 'object' ? key : { [key]: value }
 
     for (const [name, val] of Object.entries(key)) {
-      Deep.set(this.#items, name, val)
+      set(this.#items, name, val)
     }
 
     return this
